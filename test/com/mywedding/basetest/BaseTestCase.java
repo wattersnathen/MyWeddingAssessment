2e7c933f06521134d0d9fa0642f9343d38b18f3d
Nathen Watters <wattersnathen@gmail.com>
2015-08-12 15:49:34 -0600

    Adding a FirefoxProfile option during test setup to have the browser allow user to share location

--- test/com/mywedding/basetest/BaseTestCase.java
+++ test/com/mywedding/basetest/BaseTestCase.java
@@ -6,6 +6,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.firefox.internal.ProfilesIni;
 
 public class BaseTestCase {
 	
@@ -13,7 +15,20 @@ public class BaseTestCase {
 	
 	@Before
 	public void setup() {
-		driver = new FirefoxDriver();
+		
+		ProfilesIni profile = new ProfilesIni();
+		
+		// Use a FirefoxProfile that was configured to always allowed to share location
+		FirefoxProfile myProfile = profile.getProfile("TestAutomation");
+		
+		if (myProfile != null) {
+			driver = new FirefoxDriver(myProfile);
+		}
+		else {
+			// no such profile currently exists, use a default FirefoxDriver instead
+			driver = new FirefoxDriver();
+		}
+		
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 	}
@@ -23,5 +38,10 @@ public class BaseTestCase {
 		if (driver != null) {
 			driver.quit();
 		}
+		/*
+		 * If org.openqa.selenium.os.UnixProcess$SeleniumWatchDog destroyHarder warning shows up
+		 * in the log/console, consider downgrading your current version of Firefox, otherwise 
+		 * the tests should still work.
+		 */
 	}
 }
