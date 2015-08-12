35a11837707686920cc1d8c9d8e8fed88fca2e35
Nathen Watters <wattersnathen@gmail.com>
2015-08-12 17:23:05 -0600

    Added FindVendorPage support for the category dropdown button

--- src/com/mywedding/pages/FindVendorPage.java
+++ src/com/mywedding/pages/FindVendorPage.java
@@ -1,6 +1,8 @@
 package com.mywedding.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class FindVendorPage extends Page {
@@ -9,9 +11,16 @@ public class FindVendorPage extends Page {
 	
 	protected static final String FINDVENDORPAGE_URL = "http://www.mywedding.com/home/find-vendor";
 	
+	@FindBy(css="#category button")
+	private WebElement dropdownButton;
+	
 	public FindVendorPage(WebDriver driver) {
 		super(driver);
 		this.driver = driver;
 		PageFactory.initElements(this.driver, FindVendorPage.this);
+	}
+	
+	public String getDropdownButtonForCategoriesText() {
+		return dropdownButton.getText();
 	}
 }
