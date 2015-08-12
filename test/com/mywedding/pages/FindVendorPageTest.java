b36e51e6ddc5f71573c7ea5f5d8720a4415c7a11
Nathen Watters <wattersnathen@gmail.com>
2015-08-12 16:55:08 -0600

    Test case for scenario one: -> Navigate to the Find Vendors Page -> Attempt to search for the ideas-search element, and check its presence

--- test/com/mywedding/pages/FindVendorPageTest.java
+++ test/com/mywedding/pages/FindVendorPageTest.java
@@ -2,6 +2,8 @@ package com.mywedding.pages;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 import com.mywedding.basetest.BaseTestCase;
 
@@ -18,5 +20,24 @@ public class FindVendorPageTest extends BaseTestCase {
 		Assert.assertEquals(FindVendorPage.FINDVENDORPAGE_URL, vendorPage.getURL());
 	}
 	
-
+	/**
+	 * The ideas search section is <a href="http://prntscr.com/83e3tq"> this</a>, and we 
+	 * only want that to be present on the "Collect Ideas" tab, not the "Find Vendors" tab.
+	 */
+	@Test
+	public void ideasSearchSectionIsNotPresentOnTheFindVendorsPage() {
+		/* Given:
+		 * 		I am on the find vendor home page
+		 */
+		HomePage homePage = new HomePage(driver).navigateToHomePage();
+		FindVendorPage vendorPage = homePage.navigateToFindVendorPage();
+		Assert.assertEquals(FindVendorPage.FINDVENDORPAGE_URL, vendorPage.getURL());
+		
+		/* Then:
+		 * 		the ideas search section is not present on the find vendor home page
+		 */
+		WebElement ideasSearch = driver.findElement(By.id("ideas-search"));
+		Assert.assertFalse("ideas-search section should not be present on the Find Vendors Page", 
+				ideasSearch.isDisplayed());			
+	}
 }
