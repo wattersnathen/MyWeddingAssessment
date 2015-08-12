7a2bef0ddb9a515d70117b8e10223dfa4b4c32a9
Nathen Watters <wattersnathen@gmail.com>
2015-08-12 15:47:58 -0600

    Adding an Abstract Page class for all Page objects to inherit from. Will contain methods and utilities for common Page operations

---
+++ src/com/mywedding/pages/Page.java
@@ -0,0 +1,26 @@
+package com.mywedding.pages;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Abstract base class for all Page classes. 
+ * 
+ * @author Nathen
+ *
+ */
+public abstract class Page {
+	
+	protected WebDriver driver;
+	
+	protected Page(final WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	public String getURL() {
+		return driver.getCurrentUrl();
+	}
+	
+	public String getTitle() {
+		return driver.getTitle();
+	}
+}
