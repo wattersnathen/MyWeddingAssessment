679b407a59ad5185db5d0a3d7d7c073bd9770959
Nathen Watters <wattersnathen@gmail.com>
2015-08-12 14:55:14 -0600

    Added HomePage class. Page that user's see on initial page visit

---
+++ src/com/mywedding/pages/HomePage.java
@@ -0,0 +1,44 @@
+package com.mywedding.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+/**
+ * HomePage class used to represent the page that first loads
+ * when a user navigates to 'www.mywedding.com'. 
+ * 
+ * @author Nathen
+ *
+ */
+public class HomePage {
+	
+	private WebDriver driver;
+	
+	protected static final String HOMEPAGE_URL = "http://www.mywedding.com";
+	
+	// Page Elements
+	@FindBy(id="tab-find-vendor")
+	private WebElement vendorTab;
+	
+	/**
+	 * Constructor for HomePage. Has a dependency for a WebDriver instance, and will 
+	 * setup WebElements using Selenium's PageFactory.
+	 * @param driver
+	 */
+	public HomePage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(this.driver, HomePage.this);
+	}
+	
+	/**
+	 * Simulate the user clicking the "FIND VENDORS" tab.
+	 * 
+	 * @return instance of FindVendorPage
+	 */
+	public FindVendorPage navigateToFindVendorPage() {
+		vendorTab.click();
+		return new FindVendorPage(this.driver);
+	}
+}
